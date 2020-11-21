61e7ef4ae44529d55c5bcc118e29921575aad5e3
dannyelgjl <dannyelgjl@gmail.com>
2020-11-21 15:19:02 -0300

    feature/Connecting with the API, creating and listing storages

--- web/src/pages/storage/index.js
+++ web/src/pages/storage/index.js
@@ -1,8 +1,50 @@
-import React from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import api from '../../services/api';
 
 const Storage = () => {
+  const [title, setTitle] = useState("");
+  const [storages, setStorages] = useState([]);
+  
+  const submit = useCallback(async () => {
+    const response = await api.post('categories', {
+      title
+    });
+    if (response.data) {
+      console.log(response.data);
+      setTitle("");
+    }
+  },[title]); 
+
+  useEffect(() => {
+    api.get('categories').then(response => {
+      setStorages(response.data);
+    });
+  },[storages]);
+
+
   return (
-    <h1>Storage</h1>
+    <>
+      <form>
+        <input 
+          value={title}
+          onChange={e => setTitle(e.target.value)}
+          type="text"
+        />
+        <Link onClick={submit} to="/storage" type="button">add</Link>
+      </form>
+
+      <div>
+        {storages.map(storage => (
+        <div key={storage.id}>
+           <h1>{storage.title}</h1>
+           <h1>{storage.id}</h1>
+        </div>
+          
+        ))}
+      </div>
+    </>
+
   )
 }
 
