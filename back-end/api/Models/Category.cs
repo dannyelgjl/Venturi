d4c2b8100210a57b0a9ab655d6abcbb751011b88
dannyelgjl <dannyelgjl@gmail.com>
2020-11-25 03:06:10 -0300

    feature/Implementing images for storages

--- back-end/api/Models/Category.cs
+++ back-end/api/Models/Category.cs
@@ -13,5 +13,9 @@ namespace api.Models
     [MinLength(3, ErrorMessage = "Este campo deve conter entre 3 e 60 caracteres")]
 
     public string Title { get; set; }
+
+    [Required(ErrorMessage = "Este campo é obrigatório")]
+    [MaxLength(999999999, ErrorMessage = "Este campo deve conter no máximo 999999999 caracteres")]
+    public string Image { get; set; }
   }
 }
